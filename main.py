06dd5bc2e3f1c1bc12ddde0ab9708368469c182a
ForeverTianyi <485249810@qq.com>
2021-08-09 14:03:12 +0800

    优化了一下代码结构，改用相对路径

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 from moviepy.editor import VideoFileClip
 import os, time, random, pygame
 
-class Game():
+
+class Game:
     width = 1280
     height = 720
     isExit = False
     runDir = os.getcwd()
+
     def showVideo(video):
         video.preview()
         video.close()
 
-class Resource():
-    startVideo = VideoFileClip('%s/resource/start720p.mp4' % Game.runDir)
+
+class Resource:
+    startVideo = VideoFileClip('resource/start720p.mp4')
     startVideo.size = [1280, 720]
 
-    icon = pygame.image.load("%s/resource/icon.png"%Game.runDir)
+    icon = pygame.image.load('resource/icon.png')
 
-class Window():
+
+class Window:
     def __init__(self):
         self.game = Game
         self.resource = Resource
@@ -25,11 +29,10 @@ class Window():
         pygame.init()
         os.environ['SDL_VIDEO_WINDOW_POS'] = '{x},{y}'.format(x=80, y=50)
         self.window = pygame.display.set_mode((Game.width, Game.height))
-        pygame.display.set_caption("Perfect Fall")
+        pygame.display.set_caption('Perfect Fall')
         pygame.display.set_icon(self.resource.icon)
         self.window.fill((255, 255, 255))
         self.game.showVideo(self.resource.startVideo)
-
 
     def loop(self):
         while self.game.isExit == False:
@@ -37,7 +40,7 @@ class Window():
             self.handlerEvent()
 
     def renderText(self, fontName, textSize, textColor, text, position):
-        TextFont = pygame.font.Font('%s/resource/fonts/%s'%(self.game.runDir, fontName), textSize)
+        TextFont = pygame.font.Font('resource/fonts/%s' % fontName, textSize)
         newText = TextFont.render(text, True, textColor)
         self.window.blit(newText, position)
 
@@ -46,6 +49,7 @@ class Window():
             if event.type == pygame.QUIT:
                 self.game.isExit = True
                 pygame.quit()
+
 
 if __name__ == '__main__':
     window = Window()

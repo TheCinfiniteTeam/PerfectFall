e2c744baed1193ecd94f0ae0fcc3e5292a4d539f
chenjj100419 <61618153+chenjj100419@users.noreply.github.com>
2021-08-10 15:58:16 +0800

    添加Font并且分类，设定FPS为60FPS并且添加了test.png测试界面.

--- main.py
+++ main.py
@@ -7,13 +7,6 @@ from resource import Resource
 pygame.init()
 pygame.mixer.init()
 
-resource = Resource(Game.runDir)
-
-pygame.mixer.music.load(resource.getPath('music', 'start'))
-
-startVideo = moviepy.editor.VideoFileClip(resource.getPath('video', 'start_720p'))
-startVideo.size = [1280, 720]
-
 class Game():
     STATES = ['NO_ENTER', 'ENTER', 'PLAY', 'END']
     STATE = STATES[0]
@@ -24,7 +17,12 @@ class Game():
         video.preview()
         video.close()
 
-icon = pygame.image.load('%s/resource/icon.png'%Game.runDir)
+resource = Resource(Game.runDir)
+
+startVideo = moviepy.editor.VideoFileClip(resource.getPath('video', 'start_720p'))
+startVideo.size = [1280, 720]
+
+icon = pygame.image.load(resource.getPath('image', 'icon'))
 
 os.environ['SDL_VIDEO_WINDOW_POS'] = '{x},{y}'.format(x=80, y=50)
 window = pygame.display.set_mode((Game.width, Game.height))
@@ -49,6 +47,9 @@ def handlerEvent():
 
 def start():
     Game.showVideo(startVideo)
+    pygame.mixer.music.load(resource.getPath('music', 'start'))
+
+gameClock = pygame.time.Clock()
 
 start()
 
@@ -58,6 +59,8 @@ while True:
             pygame.mixer.music.play()
     if Game.STATE == Game.STATES[1]:
         pygame.mixer.music.stop()
-
+    gameClock.tick(60)
+    #with open(file="fpsData.txt", encoding="utf-8", mode="a+") as fpsData:
+    #    fpsData.write("%d -|- %d/60fps -|- %d\n"%(time.time(), gameClock.get_fps(), gameClock.get_time()))
     pygame.display.update()
     handlerEvent()

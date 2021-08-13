a38c04df33c93cf3706752e6153fd79504ab230e
chenjj100419 <61618153+chenjj100419@users.noreply.github.com>
2021-08-13 22:00:11 +0800

    添加Note制作主菜单添加字体修复垂直同步的特性

--- main.py
+++ main.py
@@ -9,14 +9,15 @@ pygame.mixer.init()
 
 logger = Logger()
 
+playerUUID = getUUID('admin')
+logger.info('GET UUID > %s'%playerUUID)
+
 try:
     URL = 'http://perfectfall.cinfinitestudio.xyz'
     rp = requests.get(URL)
-    #print('online Mode %s'%rp.text)
     logger.info('Online Mode > %s'%rp.text)
     title = 'Perfect Fall [Online]'
 except Exception as exc:
-    #print("offline Mode %s"%exc)
     logger.error('Offline Mode > %s'%exc)
     title = 'Perfect Fall [Offline]'
 
@@ -24,6 +25,7 @@ except Exception as exc:
 
 class Game():
     STATES = ['NO_ENTER', 'ENTERING','MENU', 'PLAY', 'END']
+    GAMESTATES = ['LEAVE', 'IN', 'END']
     STATE = STATES[0]
     width = 1280
     height = 720
@@ -38,18 +40,17 @@ resource = Resource(Game.runDir)
 startVideo = VideoFileClip(resource.getPath('video', 'start_720p'))
 startVideo.size = [1280, 720]
 
-icon = pygame.image.load(resource.getPath('image', 'icon'))
-
 os.environ['SDL_VIDEO_WINDOW_POS'] = '{x},{y}'.format(x=80, y=50)
-window = pygame.display.set_mode((Game.width, Game.height))
+window = pygame.display.set_mode((Game.width, Game.height),vsync=True)
 pygame.display.set_caption(title)
-pygame.display.set_icon(icon)
+pygame.display.set_icon(pygame.image.load(resource.getPath('image', 'icon')))
 
 class Images():
     testImg = pygame.image.load(resource.getPath('image', 'test')).convert()
     startImg = pygame.image.load(resource.getPath('image', 'start')).convert_alpha()
 
     buttonImg = pygame.image.load(resource.getPath('image', 'button')).convert_alpha()
+    buttonDownImg = pygame.image.load(resource.getPath('image', 'button')).convert_alpha()
 
     peopleImg = pygame.image.load(resource.getPath('image', 'people')).convert_alpha()
 
@@ -63,27 +64,9 @@ def renderText(fontPath, textSize, textColor, text, position, alpha=255):
     newText.set_alpha(alpha)
     window.blit(newText, position)
 
-def handlerEvent():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            Game.STATE = Game.STATES[4]
-            pygame.quit()
-            with open(file='logs/%d.log'%time.time(), mode='a+', encoding='utf-8') as log:
-                for logl in logger.logs:
-                    log.write(logl+'\n')
-            sys.exit()
-        if Game.STATE == Game.STATES[0]:
-            if event.type == pygame.KEYDOWN:
-                logger.info('Player DOWNKEY %d'%event.key)
-                Game.STATE = Game.STATES[1]
-
-def start():
-    Game.showVideo(startVideo)
-
 gameClock = pygame.time.Clock()
 
-
-start()
+Game.showVideo(startVideo)
 
 startImgAlpha = 255
 
@@ -109,8 +92,25 @@ while not Game.STATE == Game.STATES[4]:
             Game.STATE = Game.STATES[2]
 
     if Game.STATE == Game.STATES[2]:
-        pass
+        window.blit(Images.buttonImg, (500, 248))
+        renderText(resource.getPath('font', 'Torus'), 40, (135, 206, 250), 'Solo', (600, 248))
+        window.blit(Images.buttonImg, (500, 328))
+        renderText(resource.getPath('font', 'Torus'), 40, (135, 206, 250), 'Multi', (600, 328))
+        window.blit(Images.buttonImg, (500, 408))
+        renderText(resource.getPath('font', 'Torus'), 40, (135, 206, 250), 'Configure', (552, 408))
 
+    #Event Handler
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            Game.STATE = Game.STATES[4]
+            pygame.quit()
+            with open(file='logs/%d.log'%time.time(), mode='a+', encoding='utf-8') as log:
+                for logLine in logger.logs:
+                    log.write(logLine+'\n')
+            sys.exit()
+        if Game.STATE == Game.STATES[0]:
+            if event.type == pygame.KEYDOWN:
+                logger.info('Player DOWNKEY %d'%event.key)
+                Game.STATE = Game.STATES[1]
     gameClock.tick(60)
     pygame.display.update()
-    handlerEvent()
